1979cd8f2aba3e431bc5a2d969f59d9930e07bbf
Kristina Borisova <kodzuken26@yandex.ru>
2026-03-17 17:50:27 +0400

    header done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,37 +1,37 @@
 import logoLightImage from "../../assets/images/logo_light.png";
 import darkBtnImage from "../../assets/images/dark_btn.png";
+import logoDarkImage from "../../assets/images/logo_dark.png";
+import lightBtnImage from "../../assets/images/light_btn.png";
 
 import "./header.scss";
 import { useContext } from "react";
-import { ThemeContext } from '../../context/ThemeContext';
+import { ThemeContext } from "../../context/ThemeContext";
 
 function Header() {
   const context = useContext(ThemeContext);
 
-  
-  if (!context) return null; 
-  
-  const { theme, toggleTheme } = context;
+  if (!context) return null;
 
-  
+  const { theme, toggleTheme } = context;
 
   return (
     <>
       <div className={`header-block ${theme}`}>
-        <img src={logoLightImage} alt="logo-light" className="logo" />
-        <button onClick={toggleTheme}>
-          <img
-            src={darkBtnImage}
-            id="btn"
-            alt="moon-btn "
-            
-          />
-        </button>
+        <img
+          src={theme === "light" ? logoLightImage : logoDarkImage}
+          alt="logo-light"
+          className="logo"
+        />
+        <img
+          src={theme === "light" ? darkBtnImage : lightBtnImage}
+          id="btn"
+          alt="moon-btn "
+          onClick={toggleTheme}
+          className="btn-theme"
+        />
       </div>
     </>
   );
 }
 
 export default Header;
-
-
