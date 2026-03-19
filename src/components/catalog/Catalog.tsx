8115274c5ec7cd8cf889b61a1ff48abdddabf4a6
Kristina Borisova <kodzuken26@yandex.ru>
2026-03-19 21:12:24 +0400

    got pictures

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -1,30 +1,46 @@
 import { useEffect, useState, type FC } from "react";
-import type { IPaintings } from "../types/types";
+import type { IPaintings, IAuthors, ILocations } from "../types/types";
 import axios from "axios";
+import "./catalog.scss"
 
 const API_PAINTINGS = 'https://test-front.framework.team/paintings'
+const API_AUTHORS = 'https://test-front.framework.team/authors'
+const API_LOCATIONS = 'https://test-front.framework.team/locations'
 
 interface CatalogProps {
     initialPaintings?: IPaintings[];
+    initialAuthors?: IAuthors[];
+    initialLocations?: ILocations[];
 }
 
-const Catalog: FC<CatalogProps> = ({ initialPaintings = [] }) => {
+const Catalog: FC<CatalogProps> = ({ initialPaintings = [], initialAuthors = [], initialLocations = [] }) => {
     const [paintings, setPaintings] = useState<IPaintings[]>(initialPaintings)
+    const [authors, setAuthors] = useState<IAuthors[]>(initialAuthors)
+    const [locations, setLocations] = useState<ILocations[]>(initialLocations)
 
     useEffect(() => {
         const fetchPaintings = async () => {
             const response = await axios.get<IPaintings[]>(API_PAINTINGS);
             setPaintings(response.data);
+            const responseAuthor = await axios.get<IAuthors[]>(API_AUTHORS);
+            setAuthors(responseAuthor.data);
+            const responseLocation = await axios.get<ILocations[]>(API_LOCATIONS);
+            setLocations(responseLocation.data)
         }
         fetchPaintings()
     }, [])
+
     return (
         <>
             <div className="catalog-block">
                 {paintings.map(element => (
                     <div key={element.id} className="painting-card">
-                        <img src={element.imgUrl} alt={element.name} />
+                        {/* <img src={`https://test-front.framework.team/${element.imgUrl}`} alt={element.name} /> */}
+                        {/* <img src={"https://test-front.framework.team/" + element.imgUrl} alt={element.name} /> */}
+                        <img src={`https://test-front.framework.team${element.imageUrl}`} alt={element.name} className="painting-image" />
+                        <p>{element.authorid}</p>
                         <p>{element.name}</p>
+                        <p>{element.created}</p>
                 </div>
             ))}
             </div>
--- src/components/types/types.tsx
+++ src/components/types/types.tsx
@@ -2,7 +2,17 @@ export interface IPaintings{
     authorid: number;
     created: number;
     id: number;
-    imgUrl: string;
+    imageUrl: "string";
     locationId: number;
     name: string;
+}
+
+export interface IAuthors{
+    id: number;
+    name: string;
+}
+
+export interface ILocations{
+    id: number;
+    location: string;
 }
